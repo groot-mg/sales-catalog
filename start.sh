b18dae6d24987b9cfa2875e8de6f1b4b94525f7e
Mauricio Generoso <mauro_generoso@hotmail.com>
2022-05-08 10:55:53 +0100

    Set local as default profile, remove unnecessary dependency versions and update script to start docker-compose

--- start.sh
+++ start.sh
@@ -1,5 +1,13 @@
-./gradlew clean build && \
+#!/bin/sh
 
-docker build spring-crud-api/ && \
+MAIN_MODULE_IMAGE_ID=$(docker images -aq spring-crud-app_app)
+if [[ ! -z "${MAIN_MODULE_IMAGE_ID}" ]]; then
+  echo "Removing previously MAIN_MODULE_IMAGE with id: ${MAIN_MODULE_IMAGE_ID}"
+  docker rm $(docker rmi -f ${MAIN_MODULE_IMAGE_ID})
+fi
 
+echo "Building project"
+./gradlew clean build
+
+echo "Starting Docker Compose"
 docker-compose up --remove-orphans
