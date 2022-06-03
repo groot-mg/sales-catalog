a6c04e8b1168eee164a672cc38fe18b7664a571b
Mauricio Generoso <mauro_generoso@hotmail.com>
2022-06-03 01:48:56 +0100

    feat(#26): Introduce service-discovery

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-MAIN_MODULE_IMAGE_ID=$(docker images -aq spring-shopping-api_app)
+MAIN_MODULE_IMAGE_ID=$(docker images -aq spring-shopping-api_shopping-api)
 if [[ ! -z "${MAIN_MODULE_IMAGE_ID}" ]]; then
   echo "Removing previously MAIN_MODULE_IMAGE with id: ${MAIN_MODULE_IMAGE_ID}"
   docker rm $(docker rmi -f ${MAIN_MODULE_IMAGE_ID})
@@ -12,8 +12,15 @@ if [[ ! -z "${MAIN_MODULE_IMAGE_ID}" ]]; then
   docker rm $(docker rmi -f ${MAIN_MODULE_IMAGE_ID})
 fi
 
+MAIN_MODULE_IMAGE_ID=$(docker images -aq spring-shopping-api_service-discovery)
+if [[ ! -z "${MAIN_MODULE_IMAGE_ID}" ]]; then
+  echo "Removing previously MAIN_MODULE_IMAGE with id: ${MAIN_MODULE_IMAGE_ID}"
+  docker rm $(docker rmi -f ${MAIN_MODULE_IMAGE_ID})
+fi
+
 echo "Building project"
 ./gradlew clean build
 
 echo "Starting Docker Compose"
+#docker-compose up --remove-orphans shopping-api service-discovery database
 docker-compose up --remove-orphans
