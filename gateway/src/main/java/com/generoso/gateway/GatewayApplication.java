0c25e4bf9235933fd53bd8f346409da86dd93f6d
Mauricio Generoso <mauro_generoso@hotmail.com>
2022-07-08 22:06:19 +0100

    feat(#29): replace zuul by spring cloud gateway

--- gateway/src/main/java/com/generoso/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/generoso/gateway/GatewayApplication.java
@@ -3,11 +3,9 @@ package com.generoso.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringBootApplication
-@EnableZuulProxy
 @EnableEurekaClient
+@SpringBootApplication
 public class GatewayApplication {
 
     public static void main(String[] args) {
