2376af9afae53fda32668d23dcb3aaa0e8a1f4af
Mauricio Generoso <mauro_generoso@hotmail.com>
2022-07-08 02:18:05 +0100

    feat(#26): add discovery-service and gateway

--- docker-compose.sh
+++ docker-compose.sh
@@ -18,9 +18,15 @@ if [[ ! -z "${MAIN_MODULE_IMAGE_ID}" ]]; then
   docker rm $(docker rmi -f ${MAIN_MODULE_IMAGE_ID})
 fi
 
+MAIN_MODULE_IMAGE_ID=$(docker images -aq spring-shopping-api_gateway)
+if [[ ! -z "${MAIN_MODULE_IMAGE_ID}" ]]; then
+  echo "Removing previously MAIN_MODULE_IMAGE with id: ${MAIN_MODULE_IMAGE_ID}"
+  docker rm $(docker rmi -f ${MAIN_MODULE_IMAGE_ID})
+fi
+
 echo "Building project"
 ./gradlew clean build
 
 echo "Starting Docker Compose"
-#docker-compose up --remove-orphans shopping-api service-discovery database
-docker-compose up --remove-orphans
+#docker-compose up --build --force-recreate shopping-api service-discovery database gateway
+docker-compose up --build --force-recreate
